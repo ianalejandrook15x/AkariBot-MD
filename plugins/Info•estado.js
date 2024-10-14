eaebfc26459e76956d2ba922194df1e93c71ab19
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 11:27:32 -0300

    Update Info•estado.js

--- plugins/Info•estado.js
+++ plugins/Info•estado.js
@@ -14,7 +14,7 @@ let muptime = clockString(_muptime)
 const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
 const used = process.memoryUsage()
-let yaemori = `╭─⬣「 *Estado De Akari Nankano* 」⬣\n`
+let yaemori = `╭─⬣「 *Estado De Akari* 」⬣\n`
 yaemori += `│ 🌸 *Creador ∙* ianalejandrook15x\n`
 yaemori += `│ 📚 *Grupos Unidos ∙* ${groupsIn.length}\n`
 yaemori += `│ 👤 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`
