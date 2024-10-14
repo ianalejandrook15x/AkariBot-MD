88ee745216a234591c979c09a5ff76eddb1b46d3
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-13 22:34:01 -0300

    A

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -5,22 +5,13 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🌸 ¡Hola! Soy Akari Nankano, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🌸 ¡Hola! Soy Akari, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
 }
 /*if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
 conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n🌸 *Dash:* https://dash.corinplus.com\n🌸 *Panel:* https://ctrl.corinplus.com`, m, rcanal, )
 }*/
-if (/^sexo$/i.test(m.text)) {
-conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
-}
-if (/^tetas|teta$/i.test(m.text)) {
-conn.reply(m.chat, `*que caliente eres* 🥵`, m, rcanal, )
-}
-if (/^bug$/i.test(m.text)) {
-conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
-}
-if (/^pene$/i.test(m.text)) {
-conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
+if (/^ian$/i.test(m.text)) {
+conn.reply(m.chat, `Hσʅα Iαɳ ҽʂ ɱι ƈɾҽαԃσɾ`, m, rcanal, )
 }
 return !0;
 };
