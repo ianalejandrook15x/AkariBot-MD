34efc262337f302a27d6cdcd6d7f4a6ccf546044
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-04 12:15:31 -0300

    Update declaracion.js

--- plugins/declaracion.js
+++ plugins/declaracion.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         const messageText = `Hola Valentina \nVengo a decirte que de hace mucho me gustas pero no fui capaz de demostrar amor y cariño, te quiero pedir disculpas por mi comportamiento en dejarte hablar\nPero con el tiempo me di cuenta que el error fue mio y quiero pedirte disculpas\nEstraño los abrazos que nos dabamos demostraban cariño, realmente quiero que me perdones y empezar otra vez. \n\n¿ Me Perdonas ?\n\n\n¿ Te gustaria salir conmigo ?`;
 
         await conn.sendButton(m.chat, messageText, 'Declaración', imageUrl, [
-            ['Si', `${usedPrefix}Si`],
-            ['No', `${usedPrefix}No`]
+            ['Si Te perdono', `${usedPrefix}Si`],
+            ['No Te perdono', `${usedPrefix}No`]
         ], m);
     }
 };
