659067caa2fe80a7195bb9b03d537fafab80a4af
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-03 23:36:04 -0300

    Update Rpg•trabajar.js

--- plugins/Rpg•trabajar.js
+++ plugins/Rpg•trabajar.js
@@ -17,7 +17,7 @@ user.exp += rsl
 handler.help = ['trabajar']
 handler.tags = ['rpg']
 handler.command = ['w','work', 'trabajar']
-handler.register = true 
+handler.register = false
 export default handler
 
 function toNum(number) {
