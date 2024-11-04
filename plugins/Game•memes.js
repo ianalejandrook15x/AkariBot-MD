1e56b564065438c089413b4df1ec440fb24a1f05
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-03 23:18:55 -0300

    Update Game•memes.js

--- plugins/Game•memes.js
+++ plugins/Game•memes.js
@@ -8,6 +8,5 @@ m.react(emoji2)
 handler.help = ['meme']
 handler.tags = ['fun']
 handler.command = ['meme', 'memes']
-handler.yenes = 1
-handler.register = true
+handler.register = false
 export default handler
