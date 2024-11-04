20d678476a2825994014de4f404c4783fe7dd077
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-03 23:04:03 -0300

    Update Buscador•tweetposts.js

--- plugins/Buscador•tweetposts.js
+++ plugins/Buscador•tweetposts.js
@@ -63,7 +63,7 @@ console.error(error)
 handler.help = ['tweetposts']
 handler.tags = ['buscador']
 handler.command = ['tweetposts']
-handler.register = true
+handler.register = false
 handler.yenes = 1
 
 export default handler;
