62ddf636a96f7b611a6bfd19b394a7b1d072b760
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-04 11:52:59 -0300

    Update Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -4,8 +4,8 @@ handler.all = async function(m) {
 
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
-if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🌸 ¡Hola! Soy Akari, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
+if (/^No te perdono$/i.test(m.text)) {
+conn.reply(m.chat, `Esta bien, agradezco tu sinceridad por haberme respondido, te deseo suerte.`, m, )
 }
 
 if (chat.isBaneed) return
@@ -13,8 +13,8 @@ if (/^reglas$/i.test(m.text)) {
 conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5493876639332\n\n> Disfrute de *AkariBot-MD Powered By Ianalejandrook15x*`, m, rcanal, )
 }
   
-if (/^ian$/i.test(m.text)) {
-conn.reply(m.chat, `Hola Ian es mi creador, si deseas unir a la bot puedes contactarte con el aqui te dejo su numero\n\n> wa.me/5493876639332`, m, rcanal, )
+if (/^Sí Te perdono$/i.test(m.text)) {
+conn.reply(m.chat, `Muchas gracias agradezco tu sinceridad por perdonarme, juro que dare todo de mi. Te quiero mucho... 😙`, m, )
 }
 return !0;
 };
