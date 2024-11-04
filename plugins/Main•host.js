940a4bd3dc7f4b6d38cb70abdd268d6985bd18dd
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-04 15:49:59 -0300

    Update Main•host.js

--- plugins/Main•host.js
+++ plugins/Main•host.js
@@ -1,40 +1,28 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-let txt = `🌟 *Sky Ultra Plus: Tu Solución de Hosting de Alto Rendimiento* 🌟
-
-🚀 *¿Buscas un hosting rápido y confiable?* Sky Ultra Plus es tu opción ideal. Ofrecemos servidores de alto rendimiento a precios accesibles, perfectos para cualquier proyecto.
-
-*Beneficios de Sky Ultra Plus:*
-⚡• *Velocidad Ultrarápida:* Tu proyecto responderá en segundos.
-🔒 • *Seguridad total:* Protección avanzada.
-📞 • *Soporte 24/7:* Estamos aquí para ayudarte en cualquier momento.
-
-🌐 *Página:*
-https://dash.skyultraplus.com
-
-¡Elige Sky Ultra Plus y lleva tu presencia online al siguiente nivel! 🌟🚀💻` 
+let txt = `Hola` 
 
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardedNewsletterMessageInfo: { 
 newsletterJid: '120363301598733462@newsletter', 
 serverMessageId: '', 
-newsletterName: '☁️ Sky Ultra Plus - Channel' }, 
+newsletterName: 'Gracias por perdonarme' }, 
 forwardingScore: 9999999,
 isForwarded: true, 
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `☁️ 𝗦𝗸𝘆 𝗨𝗹𝘁𝗿𝗮️ 𝗣𝗹𝘂𝘀 ☁️`,
-body: `¡Sky Ultra Plus, tu mejor opción! ⭐️`,
+title: `Gɾαƈιαʂ ρσɾ ρҽɾԃσɳαɾɱҽ`,
+body: `Dilan es gay`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://qu.ax/wXciz.jpg', 
+thumbnailUrl: 'https://qu.ax/VccqK.jpg', 
 sourceUrl: redeshost}}},
 { quoted: fkontak})
 } 
 
-handler.help = ['skyplus']
-handler.tags = ['main']
-handler.command = ['skyplus', 'skyultra', 'skyultraplus', 'ultraplus', 'hosting', 'host']
+handler.help = ['si']
+handler.tags = ['RPS']
+handler.command = ['si']
 
 export default handler
