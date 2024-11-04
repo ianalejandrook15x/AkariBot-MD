bdc9fd567a043528617c15fda46c0a36dd29375b
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-04 15:41:45 -0300

    Update Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -4,7 +4,7 @@ handler.all = async function(m) {
 
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
-if (/^No$/i.test(m.text)) {
+if (/^#No$/i.test(m.text)) {
 conn.reply(m.chat, `Esta bien, agradezco tu sinceridad por haberme respondido, te deseo suerte.`, m, )
 }
 
@@ -13,7 +13,7 @@ if (/^reglas$/i.test(m.text)) {
 conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5493876639332\n\n> Disfrute de *AkariBot-MD Powered By Ianalejandrook15x*`, m, rcanal, )
 }
   
-if (/^Si$/i.test(m.text)) {
+if (/^#Si$/i.test(m.text)) {
 conn.reply(m.chat, `Muchas gracias agradezco tu sinceridad por perdonarme, juro que dare todo de mi. Te quiero mucho... 😙`, m, )
 }
 return !0;
