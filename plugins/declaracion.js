695c402ad7820b76622b3e97439804a18f6c69fc
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-04 12:11:44 -0300

    Update declaracion.js

--- plugins/declaracion.js
+++ plugins/declaracion.js
@@ -4,11 +4,11 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command }) => {
     if (command === 'declaracion') {
         const imageUrl = 'https://qu.ax/lpPQ.jpg'; // URL de la imagen de la confesión
-        const messageText = `Hola Valentina \nVengo a decirte que de hace mucho me gustas pero no fui capaz de demostrar amor y cariño, te quiero pedir disculpas por mi comportamiento en dejarte hablar\nPero con el tiempo me di cuenta que el error fue mio y quiero pedirte disculpas\nEstraño los abrazos que nos dabamos demostraban cariño, realmente quiero que me perdones y empezar otra vez. \n\n¿ Me Perdonas ?\n\n`;
+        const messageText = `Hola Valentina \nVengo a decirte que de hace mucho me gustas pero no fui capaz de demostrar amor y cariño, te quiero pedir disculpas por mi comportamiento en dejarte hablar\nPero con el tiempo me di cuenta que el error fue mio y quiero pedirte disculpas\nEstraño los abrazos que nos dabamos demostraban cariño, realmente quiero que me perdones y empezar otra vez. \n\n¿ Me Perdonas ?\n\n\n¿ Te gustaria salir conmigo ?`;
 
         await conn.sendButton(m.chat, messageText, 'Declaración', imageUrl, [
-            ['Sí Te Perdono', `${usedPrefix}Si`],
-            ['No Lo Siento Mucho', `${usedPrefix}No`]
+            ['Si', `${usedPrefix}Si`],
+            ['No', `${usedPrefix}No`]
         ], m);
     }
 };
