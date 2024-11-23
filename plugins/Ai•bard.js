966cd453e9ab6f2a7130600c7f7ff8f8529cfcc1
ianalejandrook15x <sm.iam1836@gmail.com>
2024-11-23 12:13:15 -0300

    Update Ai•bard.js

--- plugins/Ai•bard.js
+++ plugins/Ai•bard.js
@@ -16,6 +16,6 @@ return conn.reply(m.chat, '🌸 *Ocurrió un fallo*', m, rcanal)
 
 handler.command = ['bard']
 handler.help = ['bard']
-handler.tags = ['ia']
+handler.tags = ['ai']
 handler.premium = false
 export default handler
